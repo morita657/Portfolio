44f23bc6368b8a0e094981fb2577b696c2b537b4
morita657 <morita657@gmail.com>
2020-08-15 15:41:05 +0900

    Update greeting message

--- src/About.vue
+++ src/About.vue
@@ -1,30 +1,31 @@
 <template>
-<div>
-    <h1>About</h1>
-    <div class="about">
-        <div class="introduction">
-            <p>Hello, My name is Takahiro Morita.</p>
-            <p>I am an industrial engineer who found my true passion in programming. While working as a SEO specialist in Thailand, I taught myself coding by joining online courses.
-            Currently I am at Code Chrysalis creating full-stack applications with technologies such as React, Node.js, Knex, Postgres and Heroku. At present, I am building an Android service using Kotlin.</p>
-            <p>During my off time, I like to read books about topics I am curious in such as the mechanism of mobile phone and the history of cipher. And, also exploring new technologies with Arduino to build small IoT service with JavaScript.</p>
+  <div>
+    <div class="container">
+      <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-12 px-0">
+          <h1 class="display-4">Takahiro Morita</h1>
+          <p class="lead my-3">
+            Thank you for visiting my page!<br />
+            I am a Computer Science Student at Arizona State University. <br />
+            As a Computer Engineer, I have a professional experience of three
+            years working for web, mobile and cloud marketing solution
+            development in the Information Technology industry. Please explore
+            my projects below.
+          </p>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
-.about {
-  width: 100vw;
-  height: 530px;
-  text-align: left;
-}
 h1 {
   color: #f9812a;
+  text-align: left;
 }
-.introduction {
-  margin: 0 30px;
+p {
+  text-align: left;
 }
 </style>
